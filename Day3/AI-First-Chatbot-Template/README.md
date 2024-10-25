66457571400fcbd288cf0ab45bbfcf96bef93ecd
Amber Teng <amber@boostkh.com>
2024-10-26 00:23:15 +0800

    adding ALGOREX-PH Code from https://github.com/ALGOREX-PH/AI-First-Chatbot-Template
    Adding Danielle's code

---
+++ Day3/AI-First-Chatbot-Template/README.md
@@ -0,0 +1,1 @@
+# AI-First-Chatbot-Template
---
+++ Day3/AI-First-Chatbot-Template/app.py
@@ -0,0 +1,75 @@
+import os
+import openai
+import numpy as np
+import pandas as pd
+import json
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from openai.embeddings_utils import get_embedding
+import faiss
+import streamlit as st
+import warnings
+from streamlit_option_menu import option_menu
+from streamlit_extras.mention import mention
+
+warnings.filterwarnings("ignore")
+
+st.set_page_config(page_title="AI First Chatbot Template", page_icon="", layout="wide")
+
+with st.sidebar :
+    st.image('images/White_AI Republic.png')
+    openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+    if not (openai.api_key.startswith('sk-') and len(openai.api_key)==164):
+        st.warning('Please enter your OpenAI API token!', icon='⚠️')
+    else:
+        st.success('Proceed to entering your prompt message!', icon='👉')
+    with st.container() :
+        l, m, r = st.columns((1, 3, 1))
+        with l : st.empty()
+        with m : st.empty()
+        with r : st.empty()
+
+    options = option_menu(
+        "Dashboard", 
+        ["Home", "About Us", "Model"],
+        icons = ['book', 'globe', 'tools'],
+        menu_icon = "book", 
+        default_index = 0,
+        styles = {
+            "icon" : {"color" : "#dec960", "font-size" : "20px"},
+            "nav-link" : {"font-size" : "17px", "text-align" : "left", "margin" : "5px", "--hover-color" : "#262730"},
+            "nav-link-selected" : {"background-color" : "#262730"}          
+        })
+
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+if 'chat_session' not in st.session_state:
+    st.session_state.chat_session = None  # Placeholder for your chat session initialization
+
+# Options : Home
+if options == "Home" :
+
+   st.title("This is the Home Page!")
+   st.write("Intorduce Your Chatbot!")
+   st.write("What is their Purpose?")
+   st.write("What inspired you to make [Chatbot Name]?")
+   
+elif options == "About Us" :
+     st.title("About Us")
+     st.write("# [Name]")
+     st.image('images/Meer.png')
+     st.write("## [Title]")
+     st.text("Connect with me via Linkedin : [LinkedIn Link]")
+     st.text("Other Accounts and Business Contacts")
+     st.write("\n")
+
+# Options : Model
+elif options == "Model" :
+     st.title("This Section is for your Chatbot!")
